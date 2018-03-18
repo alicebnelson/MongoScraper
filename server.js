cc3ac834ed5c77e0d622413235a2e7f66022c74c
alicebnelson <nelsonaliceb@gmail.com>
2018-03-18 18:58:06 -0500

    styling changes

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.static(process.cwd() + "/public"));
 // Database configuration with mongoose
 var databaseUri = "mongodb://localhost/mongoosearticles";
 
+
 if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 } else {
